a95c196357727d93077018fdc0ad9f46b6f58ea6
Craig Andera <candera@wangdera.com>
2009-04-09 10:24:39 -0400

    * Removed line detection * Added smoothing and contour detection

--- wcpad.cpp
+++ wcpad.cpp
@@ -3,89 +3,70 @@
 
 #include "stdafx.h"
 
-int g_edgeThreshold1 = 10; 
-int g_edgeThreshold2 = 15; 
-int g_lineRho = 1; 
-int g_lineTheta = 1;
-int g_lineThreshold = 50; 
-int g_lineParam1 = 30; 
-int g_lineParam2 = 5; 
 int g_threshold = 5; 
+int g_erosions = 1;
+int g_dilations = 1; 
+int g_maxDepth = 5;
 
 CvCapture* g_capture; 
 IplImage* g_raw; 
-IplImage* g_background; 
-IplImage* g_edges; 
-IplImage* g_grey; 
-IplImage* g_lines; 
+IplImage* g_grabbed; 
 IplImage* g_thresholded; 
-CvMemStorage* g_lineStorage; 
+IplImage* g_morphed; 
+IplImage* g_contours; 
+IplConvKernel* g_morphKernel; 
+CvMemStorage* g_storage; 
 
-void update_edges(int)
+void update_contours(int)
 {
-	cvCanny(g_background, g_edges, g_edgeThreshold1, g_edgeThreshold2); 
-	cvShowImage("edges", g_edges); 
+	cvScale(g_grabbed, g_contours, 0.5); 
 
-	CvSeq* lines = cvHoughLines2(
-		g_edges, 
-		g_lineStorage, 
-		CV_HOUGH_PROBABILISTIC, 
-		(double) g_lineRho, 
-		g_lineTheta * 3.14/180.0, 
-		g_lineThreshold, 
-		g_lineParam1, 
-		g_lineParam2); 
+	CvSeq* first_contour; 
+	cvFindContours(g_morphed, g_storage, &first_contour); 
+	printf("Found %d contours\n", first_contour->total);
+	cvDrawContours(g_contours, first_contour, cvScalar(255, 255, 0), cvScalar(255, 0, 255), g_maxDepth); 
 
-	int numLines = lines->total; 
-	printf("Found %d lines\n", numLines); 
+	cvShowImage("contours", g_contours); 
+}
 
-	cvCvtColor(g_background, g_lines, CV_GRAY2RGB); 
-	cvConvertScale(g_lines, g_lines, 0.5); 
-	//cvCopy(g_raw, g_lines); 
+void update_morphs(int)
+{
+	cvCopy(g_thresholded, g_morphed); 
 
-	for (int i = 0; i < numLines; ++i)
+	if (g_dilations > 0)
 	{
-		CvPoint* line = (CvPoint*) cvGetSeqElem(lines, i); 
-		cvDrawLine(g_lines, line[0], line[1], cvScalar(255, 255, 0));
+		cvDilate(g_morphed, g_morphed, g_morphKernel, g_dilations); 
+	}
+	if (g_erosions > 0)
+	{
+		cvErode(g_morphed, g_morphed, g_morphKernel, g_erosions); 
 	}
 
-	cvShowImage("lines", g_lines); 
+	cvShowImage("morphed", g_morphed); 
+	update_contours(0); 
 }
 
 void update_threshold(int)
 {
-	cvCvtColor(g_raw, g_thresholded, CV_RGB2GRAY); 
 	//cvThreshold(g_thresholded, g_thresholded, g_threshold, 255, CV_THRESH_BINARY); 
-	cvAdaptiveThreshold(g_thresholded, g_thresholded, 255, 0, 0, 3, g_threshold); 
+	cvCvtColor(g_grabbed, g_thresholded, CV_RGB2GRAY); 
+	cvAdaptiveThreshold(g_thresholded, g_thresholded, 255, 0, CV_THRESH_BINARY_INV, 3, g_threshold); 
 	cvShowImage("thresholded", g_thresholded); 
+	update_morphs(0); 
 }
 
 void grab()
 {
-	g_raw = cvQueryFrame(g_capture); 
-	cvCvtColor(g_raw, g_grey, CV_BGR2GRAY); 
+	cvCopy(g_raw, g_grabbed); 
+	cvSmooth(g_grabbed, g_grabbed); 
+	cvShowImage("grabbed", g_grabbed); 
+	update_threshold(0); 
 }
 
 void update()
 {
-	grab(); 
-	update_threshold(0); 
+	g_raw = cvQueryFrame(g_capture); 
 	cvShowImage("raw", g_raw); 
-	cvShowImage("grey", g_grey);
-}
-void grab_background()
-{
-	cvZero(g_background); 
-	int iterations = 10; 
-	for (int i = 0; i < iterations; ++i)
-	{
-		grab(); 
-		cvAddWeighted(g_grey, 1.0/(float)iterations, g_background, 1.0, 0, g_background); 
-	}
-	
-	cvShowImage("background", g_background); 
-	update_edges(0); 
-	update_threshold(0); 
 }
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -93,33 +74,27 @@ int _tmain(int argc, _TCHAR* argv[])
 	g_capture = cvCreateCameraCapture(0); 
 
 	cvNamedWindow("raw"); 
-	cvNamedWindow("background");
-	cvNamedWindow("grey"); 
-    cvNamedWindow("edges");
-	cvNamedWindow("lines"); 
+	cvNamedWindow("grabbed"); 
 	cvNamedWindow("thresholded"); 
+	cvNamedWindow("morphed"); 
+	cvNamedWindow("contours"); 
 
 	g_raw = cvQueryFrame(g_capture); 
-	g_grey = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
-    g_background = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
-	g_edges = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
-	g_lines = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 3); 
+	g_grabbed = cvCloneImage(g_raw); 
 	g_thresholded = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
+	g_morphed = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
+	g_contours = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 3); 
 
-	g_lineStorage = cvCreateMemStorage();
+	g_morphKernel = cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT); 
 
-	cvCreateTrackbar("threshold1", "edges", &g_edgeThreshold1, 255, update_edges); 
-	cvCreateTrackbar("threshold2", "edges", &g_edgeThreshold2, 255, update_edges); 
-	cvCreateTrackbar("rho", "lines", &g_lineRho, 255, update_edges); 
-	cvCreateTrackbar("theta", "lines", &g_lineTheta, 255, update_edges); 
-	cvCreateTrackbar("threshold", "lines", &g_lineThreshold, 255, update_edges); 
-	cvCreateTrackbar("param1", "lines", &g_lineParam1, 255, update_edges); 
-	cvCreateTrackbar("param2", "lines", &g_lineParam2, 255, update_edges); 
+	g_storage = cvCreateMemStorage(); 
+
 	cvCreateTrackbar("threshold", "thresholded", &g_threshold, 255, update_threshold); 
+	cvCreateTrackbar("erosions", "morphed", &g_erosions, 10, update_morphs); 
+	cvCreateTrackbar("dilations", "morphed", &g_dilations, 10, update_morphs); 
+	cvCreateTrackbar("max_depth", "contours", &g_maxDepth, 20, update_contours); 
 
 	update(); 
-
-	// cvNamedWindow("lines"); 
 
 	printf("g = grab frame\n"); 
 	printf("\n"); 
@@ -135,7 +110,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		else if (key == 'g')
 		{
-			grab_background(); 
+			grab(); 
 		}
 		else
 		{
@@ -149,19 +124,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	cvFindContours(raw, storage, &firstContour);
 
 	cvDrawContours(scaled, firstContour, 
-    */
+    
+	cvReleaseMemStorage(&storage); 
+	*/
 
-	//cvReleaseMemStorage(&storage); 
+	// TODO: Other cleanup
 	cvReleaseCapture(&g_capture); 
-	cvReleaseImage(&g_lines); 
-	cvReleaseImage(&g_edges); 
-	cvReleaseImage(&g_grey); 
-	cvReleaseImage(&g_background);
 	cvDestroyWindow("raw"); 
-	cvDestroyWindow("background");
-	cvDestroyWindow("grey");
-	cvDestroyWindow("edges"); 
-	cvDestroyWindow("lines"); 
 	
 	return 0;
 }
