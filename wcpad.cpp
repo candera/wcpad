8a4223e641ab8676336666272cbbc0bbdd57d6eb
Craig Andera <candera@wangdera.com>
2009-04-10 11:54:12 -0400

    * Removed morphological operations - they weren't helping * Added colored contour drawing

--- wcpad.cpp
+++ wcpad.cpp
@@ -4,47 +4,54 @@
 #include "stdafx.h"
 
 int g_threshold = 5; 
-int g_erosions = 1;
-int g_dilations = 1; 
 int g_maxDepth = 5;
 
 CvCapture* g_capture; 
 IplImage* g_raw; 
 IplImage* g_grabbed; 
 IplImage* g_thresholded; 
-IplImage* g_morphed; 
 IplImage* g_contours; 
+IplImage* g_contourSource; 
 IplConvKernel* g_morphKernel; 
 CvMemStorage* g_storage; 
 
 void update_contours(int)
 {
 	cvScale(g_grabbed, g_contours, 0.5); 
+	cvCopy(g_thresholded, g_contourSource); 
 
-	CvSeq* first_contour; 
-	cvFindContours(g_morphed, g_storage, &first_contour); 
-	printf("Found %d contours\n", first_contour->total);
-	cvDrawContours(g_contours, first_contour, cvScalar(255, 255, 0), cvScalar(255, 0, 255), g_maxDepth); 
+	//cvFindContours(g_contourSource, g_storage, &contours, sizeof(CvContour), CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); 
+	//cvFindContours(g_contourSource, g_storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE); 
+	//printf("Found %d contours\n", contours->total);
+
+	CvContourScanner scanner = cvStartFindContours(g_contourSource, g_storage);
+	CvSeq* contour; 
+
+	CvScalar colors[6]; 
+	colors[0] = cvScalar(255, 0, 0);
+	colors[1] = cvScalar(0, 255, 0);
+	colors[2] = cvScalar(0, 0, 255);
+	colors[3] = cvScalar(255, 255, 0);
+	colors[4] = cvScalar(255, 0, 255);
+	colors[5] = cvScalar(0, 255, 255);
+
+	int i = 0; 
+	while ((contour = cvFindNextContour(scanner)) != NULL)
+	{
+		++i; 
+		cvDrawContours(g_contours, contour, colors[i%6], colors[(i+1)%6], 0); 
+		//CvMat* points = cvCreateMat(contour->total, 1, CV_); 
+		//cvCvtSeqToArray(contour, (void*)&(points->data)); 
+		//printf("Contour %d has area %lf\n", i, cvContourArea(points)); 
+	}
+	CvSeq* contours = cvEndFindContours(&scanner); 
+
+	//cvDrawContours(g_contours, contours, cvScalar(255, 255, 0), cvScalar(255, 0, 255), g_maxDepth, CV_FILLED); 
+	//cvDrawContours(g_contours, contours, cvScalar(255, 255, 0), cvScalar(255, 0, 255), -1, CV_FILLED, 8); 
 
 	cvShowImage("contours", g_contours); 
 }
 
-void update_morphs(int)
-{
-	cvCopy(g_thresholded, g_morphed); 
-
-	if (g_dilations > 0)
-	{
-		cvDilate(g_morphed, g_morphed, g_morphKernel, g_dilations); 
-	}
-	if (g_erosions > 0)
-	{
-		cvErode(g_morphed, g_morphed, g_morphKernel, g_erosions); 
-	}
-
-	cvShowImage("morphed", g_morphed); 
-	update_contours(0); 
-}
 
 void update_threshold(int)
 {
@@ -52,7 +59,7 @@ void update_threshold(int)
 	cvCvtColor(g_grabbed, g_thresholded, CV_RGB2GRAY); 
 	cvAdaptiveThreshold(g_thresholded, g_thresholded, 255, 0, CV_THRESH_BINARY_INV, 3, g_threshold); 
 	cvShowImage("thresholded", g_thresholded); 
-	update_morphs(0); 
+	update_contours(0); 
 }
 
 void grab()
@@ -76,22 +83,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	cvNamedWindow("raw"); 
 	cvNamedWindow("grabbed"); 
 	cvNamedWindow("thresholded"); 
-	cvNamedWindow("morphed"); 
 	cvNamedWindow("contours"); 
 
 	g_raw = cvQueryFrame(g_capture); 
 	g_grabbed = cvCloneImage(g_raw); 
 	g_thresholded = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
-	g_morphed = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
+	g_contourSource = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 1); 
 	g_contours = cvCreateImage(cvGetSize(g_raw), g_raw->depth, 3); 
 
 	g_morphKernel = cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT); 
 
 	g_storage = cvCreateMemStorage(); 
 
-	cvCreateTrackbar("threshold", "thresholded", &g_threshold, 255, update_threshold); 
-	cvCreateTrackbar("erosions", "morphed", &g_erosions, 10, update_morphs); 
-	cvCreateTrackbar("dilations", "morphed", &g_dilations, 10, update_morphs); 
+	cvCreateTrackbar("threshold", "thresholded", &g_threshold, 50, update_threshold); 
 	cvCreateTrackbar("max_depth", "contours", &g_maxDepth, 20, update_contours); 
 
 	update(); 
@@ -104,7 +108,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		int key = cvWaitKey(33); 
 		update(); 
-		if (key == 27)
+		if ((key == 27) || (key == 'q'))
 		{
 			break; 
 		}
