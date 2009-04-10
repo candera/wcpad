48266151be5e022cc2a93b68049451f10856f617
Craig Andera <candera@wangdera.com>
2009-04-10 12:27:52 -0400

    * Added code to draw contour points and polygonal approximation

--- wcpad.cpp
+++ wcpad.cpp
@@ -5,6 +5,8 @@
 
 int g_threshold = 5; 
 int g_maxDepth = 5;
+int g_contourPolyPrecision = 10; // Tenths of pixels
+int g_currentContour = 0; 
 
 CvCapture* g_capture; 
 IplImage* g_raw; 
@@ -24,9 +26,6 @@ void update_contours(int)
 	//cvFindContours(g_contourSource, g_storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE); 
 	//printf("Found %d contours\n", contours->total);
 
-	CvContourScanner scanner = cvStartFindContours(g_contourSource, g_storage);
-	CvSeq* contour; 
-
 	CvScalar colors[6]; 
 	colors[0] = cvScalar(255, 0, 0);
 	colors[1] = cvScalar(0, 255, 0);
@@ -35,19 +34,55 @@ void update_contours(int)
 	colors[4] = cvScalar(255, 0, 255);
 	colors[5] = cvScalar(0, 255, 255);
 
+#if 1
+	CvContourScanner scanner = cvStartFindContours(g_contourSource, g_storage, sizeof(CvContour), CV_RETR_CCOMP);
+	CvSeq* contour; 
+
 	int i = 0; 
+	int points = -1; 
 	while ((contour = cvFindNextContour(scanner)) != NULL)
 	{
-		++i; 
-		cvDrawContours(g_contours, contour, colors[i%6], colors[(i+1)%6], 0); 
+		if (i == g_currentContour)
+		{
+			points = contour->total;
+			cvDrawContours(g_contours, contour, colors[0], colors[1], 0); 
+
+			CvSeq* poly = cvApproxPoly(contour, sizeof(CvContour), g_storage, CV_POLY_APPROX_DP, g_contourPolyPrecision / 10.0); 
+
+			for (int j = 0; j < contour->total; ++j)
+			{
+				CvPoint* vertex = (CvPoint*) cvGetSeqElem(contour, j); 
+				cvCircle(g_contours, *vertex, 2, colors[3]); 
+			}
+			
+			for (int j = 0; j < poly->total; ++j)
+			{
+				CvPoint* vertex = (CvPoint*) cvGetSeqElem(poly, j); 
+				cvCircle(g_contours, *vertex, 2, colors[4]); 
+			}
+
+		}
 		//CvMat* points = cvCreateMat(contour->total, 1, CV_); 
 		//cvCvtSeqToArray(contour, (void*)&(points->data)); 
 		//printf("Contour %d has area %lf\n", i, cvContourArea(points)); 
+		++i; 
 	}
 	CvSeq* contours = cvEndFindContours(&scanner); 
+#else
+	CvSeq* contours; 
+	cvFindContours(g_contourSource, g_storage, &contours, sizeof(CvContour)); 
 
+	for (int i = 0; i < contours->total; ++i)
+	{
+		CvSeq* contour = (CvSeq*) cvGetSeqElem(contours, i); 
+		cvDrawContours(g_contours, contour, colors[i%6], colors[(i+1)%6], 0); 
+	}
+
+#endif
 	//cvDrawContours(g_contours, contours, cvScalar(255, 255, 0), cvScalar(255, 0, 255), g_maxDepth, CV_FILLED); 
 	//cvDrawContours(g_contours, contours, cvScalar(255, 255, 0), cvScalar(255, 0, 255), -1, CV_FILLED, 8); 
+
+	printf("Drew contour %d of %d (%d points)\n", g_currentContour, i, points); 
 
 	cvShowImage("contours", g_contours); 
 }
@@ -68,6 +103,7 @@ void grab()
 	cvSmooth(g_grabbed, g_grabbed); 
 	cvShowImage("grabbed", g_grabbed); 
 	update_threshold(0); 
+	g_currentContour = 0; 
 }
 
 void update()
@@ -97,6 +133,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	cvCreateTrackbar("threshold", "thresholded", &g_threshold, 50, update_threshold); 
 	cvCreateTrackbar("max_depth", "contours", &g_maxDepth, 20, update_contours); 
+	cvCreateTrackbar("poly precision", "contours", &g_contourPolyPrecision, 50, update_contours); 
 
 	update(); 
 
@@ -115,6 +152,16 @@ int _tmain(int argc, _TCHAR* argv[])
 		else if (key == 'g')
 		{
 			grab(); 
+		}
+		else if (key == 'n')
+		{
+			++g_currentContour; 
+			update_contours(0); 
+		}
+		else if (key == 'p')
+		{
+			--g_currentContour; 
+			update_contours(0); 
 		}
 		else
 		{
