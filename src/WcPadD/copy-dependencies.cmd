664b108debc23fb64dd04c6ea4bad186190aedbc
Craig Andera <candera@wangdera.com>
2009-09-03 12:06:00 -0400

    * Continued to massage project/build structure. I think it's mostly complete now.

---
+++ src/WcPadD/copy-dependencies.cmd
@@ -0,0 +1,13 @@
+
+@echo off
+
+REM %1 is configuration name
+REM %2 is platform name
+REM %3 is output directory
+REM %4 is project directory
+
+copy %4..\..\lib\wcpadlib\%2\%1\wcpadlib.dll %3
+copy %4..\..\lib\opencv\%1\cv110*.dll %3
+copy %4..\..\lib\opencv\%1\cvaux110*.dll %3
+copy %4..\..\lib\opencv\%1\cxcore110*.dll %3
+copy %4..\..\lib\opencv\%1\highgui110*.dll %3
