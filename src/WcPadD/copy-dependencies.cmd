a2d1b6d3e0dc0f280a4a43a6e75465e160c41503
Craig Andera <candera@wangdera.com>
2009-09-14 07:29:12 -0400

    * Updated project dependencies to fix build order problem. * Fixed problem with quoted names in cmd file

--- src/WcPadD/copy-dependencies.cmd
+++ src/WcPadD/copy-dependencies.cmd
@@ -1,13 +1,13 @@
 
-@echo off
+@echo on
 
-REM %1 is configuration name
-REM %2 is platform name
-REM %3 is output directory
-REM %4 is project directory
+REM %1 is quoted configuration name
+REM %2 is quoted platform name
+REM %3 is quoted output directory
+REM %4 is quoted project directory
 
-copy "%4..\..\lib\wcpadlib\%2\%1\wcpadlib.dll" "%3"
-copy "%4..\..\lib\opencv\%1\cv110*.dll" "%3"
-copy "%4..\..\lib\opencv\%1\cvaux110*.dll" "%3"
-copy "%4..\..\lib\opencv\%1\cxcore110*.dll" "%3"
-copy "%4..\..\lib\opencv\%1\highgui110*.dll" "%3"
+copy "%~4..\..\lib\wcpadlib\%~2\%~1\wcpadlib.dll" %3
+copy "%~4..\..\lib\opencv\%~1\cv110*.dll" %3
+copy "%~4..\..\lib\opencv\%~1\cvaux110*.dll" %3"
+copy "%~4..\..\lib\opencv\%~1\cxcore110*.dll" %3
+copy "%~4..\..\lib\opencv\%~1\highgui110*.dll" %3
